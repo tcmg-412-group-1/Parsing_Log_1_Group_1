1c135f9e677d4704e3e10cb5b7b3e35e2f23419e
Ruby Lazuli <general@patchmixolydic.com>
2023-02-04 14:31:21 -0600

    parsing_1: minor formatting fixes
    * The code that runs at the start of the script was moved to the
      bottom, below the function definitions. This makes the script easier
      to read, as the code isn't bifuracted by function definitions.
    
    * However, the definition of `url` was left at the top of the file
      since it is a constant. Further, it was renamed to `URL` to match the
      typical naming convention for constants.
    
    * `parsing_log_1` is now opened in a `with` statement like the other
      files. This ensures that it gets closed before the script ends.
    
    * Removed a space between `open` and its argument list in `write_csv`.

--- parsing_1.py
+++ parsing_1.py
@@ -5,11 +5,7 @@ import re
 from collections import Counter
 import csv
 
-url = "https://s3.amazonaws.com/tcmg476/http_access_log"
-
-r = request.get(url, stream=True)
-
-open('parsing_log_1', 'wb'). write(r.content)
+URL = "https://s3.amazonaws.com/tcmg476/http_access_log"
 
 def reader(filename):
     with open(filename) as file:
@@ -22,11 +18,16 @@ def count(time):
     return Counter(time)
 
 def write_csv(counter):
-    with open ('frequency.csv', 'w', newline='') as csvfile:
+    with open('frequency.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         header = ['Date and Time', 'Frequency']
         writer.writerow(header)
         for item in counter:
             writer.writerow((item, counter[item]))
+
+r = request.get(URL, stream=True)
+
+with open('parsing_log_1', 'wb') as log:
+    log.write(r.content)
 
 write_csv(count(reader('parsing_log_1')))
