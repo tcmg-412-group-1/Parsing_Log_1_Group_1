7985b6692b45e634d1245c33947f4b76886eed3d
Ruby Lazuli <general@patchmixolydic.com>
2023-02-05 20:29:04 -0600

    parsing_1: use the cached log file rather than downloading it again

--- parsing_1.py
+++ parsing_1.py
@@ -6,13 +6,12 @@ from collections import Counter
 import csv
 
 URL = "https://s3.amazonaws.com/tcmg476/http_access_log"
+CACHED_LOG_FILENAME = "parsing_log_1"
 
-def reader(filename):
-    with open(filename) as file:
-        log = file.read()
-        regexp = r'\d{2}\/\D{3}\/\d{4}'
-        time = re.findall(regexp, log)
-        return time
+def parse(log):
+    regexp = r'\d{2}\/\D{3}\/\d{4}'
+    time = re.findall(regexp, log)
+    return time
 
 def write_csv(counter):
     with open('frequency.csv', 'w', newline='') as csvfile:
@@ -22,9 +21,22 @@ def write_csv(counter):
         for item in counter:
             writer.writerow((item, counter[item]))
 
-r = requests.get(URL, stream=True)
+file_contents = ""
 
-with open('parsing_log_1', 'wb') as log:
-    log.write(r.content)
+try:
+    # Counterintuitively, brazenly trying to open the file and handling any
+    # resultant errors is better than checking if the file exists. Checking for
+    # existence before opening the file could lead to a time-of-check to
+    # time-of-use bug.
+    with open(CACHED_LOG_FILENAME, "r") as log:
+        file_contents = log.read()
+except FileNotFoundError:
+    with open(CACHED_LOG_FILENAME, "w") as log:
+        r = requests.get(URL, stream=True)
+        # When reading a file, Python automatically decodes the data from
+        # UTF-8 and normalizes all Windows-style newlines to Unix-style
+        # newlines. Here, we have to do it ourselves.
+        file_contents = r.content.decode().replace("\r\n", "\n")
+        log.write(file_contents)
 
-write_csv(Counter(reader('parsing_log_1')))
+write_csv(Counter(parse(file_contents)))
